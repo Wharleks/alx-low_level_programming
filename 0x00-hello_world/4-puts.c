795feefa502b27c31d5232fe6d9391fce3ab6f86
Wharleks <afagwu.dimeji@gmail.com>
2022-12-08 22:17:48 -0800

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - prints programming is like building a multilingual puzzle
+ * returns: 0 if properly exited
+ */
+int main(void)
+{
+  puts("/"Programming is like building a multilingual puzzle");
+   return (0);
+}
+
