277fbe49c2a3fc1f459338be4cd32905729311b1
Wharleks <afagwu.dimeji@gmail.com>
2022-12-12 15:37:53 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <stdin.h>
 
 /**
-* main - print the phrase with proper grammar
-* but the outcome is a pieces of art
+* main - Entry point
 *
-* Return: 0 if exited properly
+* Return: 0 (Success)
 */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
+
 return (0);
 }
