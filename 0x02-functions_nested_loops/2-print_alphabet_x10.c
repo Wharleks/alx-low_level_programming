52260c669c31670ace7d8014d337d1ad7acbf501
Wharleks <afagwu.dimeji@gmail.com>
2022-12-13 15:05:23 -0800

    prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - prints the alphabet
+* Return: 0 (Success)
+*/
+void print_alphabet(void)
+{
+char x;
+x = 'a';
+while (x <= 'z')
+{
+_putchar(x);
+x++;
+}
+_putchar('\n');
+}
