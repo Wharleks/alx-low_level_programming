58e4edfcaf5f5532007f89c31a5e05fb3ddf49ea
Wharleks <afagwu.dimeji@gmail.com>
2023-01-05 03:28:32 -0800

    natural square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,12 +11,12 @@ int power_operation(int n, int c)
 {
 	if (c % (n / c) == 0)
 	{
-		if (c * (n /c) == n)
+		if (c * (n / c) == n)
 			return (c);
 		else
 			return (-1);
 	}
-	return (0 + power_operation(n, c+ 1));
+	return (0 + power_operation(n, c + 1));
 }
 
 /**
